40ed5e2fb776df49a08d0de39bdfcd2b4abff769
DELL <DELL@SEMIH>
2021-03-12 15:18:14 +0300

    Add project files.

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //Generic Constraint
+    //class: referans tip olabilir
+    //IEntity: IEntity interface'i de olabilir. Veya: IEntity'i implemente eden bir nesne,class vs. olabilir.
+    //newlenebilir olmalı. (IEntity'i devre dışı bıraktık. IEntity'i parametre olarak veremiyoruz.)
+    //ORM, EF(EntityFramework), CRUD Operation
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -7,16 +8,29 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
+    //NuGet
     public class EfProductDal : IProductDal
     {
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            //Referansı Yakalama
+            //IDısposable pattern Implemention of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity); 
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
@@ -31,7 +45,12 @@ namespace DataAccess.Concrete.EntityFramework
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
