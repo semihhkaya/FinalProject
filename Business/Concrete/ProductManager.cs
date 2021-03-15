73f313e2f42d34d3e4f523d4d07af69cc05477e1
DELL <DELL@SEMIH>
2021-03-15 14:31:05 +0300

    Add project files.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -33,7 +33,12 @@ namespace Business.Concrete
 
         public List<Product> GetAllByCategoryId(int id)
         {
-            throw new NotImplementedException();
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByCategoryId(1))
             {
                 Console.WriteLine(product.ProductName);
             }
