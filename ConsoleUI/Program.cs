f8afedca1d82f21ea3ec89664a69aefcef5b0838
DELL <DELL@SEMIH>
2021-03-23 13:06:01 +0300

    Added project files.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,16 +5,31 @@ using System;
 
 namespace ConsoleUI
 {
-    //SOLID (O HARFİ) -OPEN CLOSED PRINCIBLE- Ar-Ge
+    //SOLID (O HARFİ) -OPEN CLOSED PRINCIBLE- Ar-Ge /// //IoC Container
     class Program
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(1))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/ " + product.CategoryName);
             }
         }
     }
